900bea7194d4dbe6d5eec0c589d4fc1db3ee9ebc
Abbat1 <cozzyyist@gmail.com>
2024-09-18 10:32:35 -0500

    glab310_1_2

---
+++ demo310_1_2/src/main/java/com/example/demo310_1_2/Demo31012Application.java
@@ -0,0 +1,13 @@
+package com.example.demo310_1_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo31012Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo31012Application.class, args);
+	}
+
+}
